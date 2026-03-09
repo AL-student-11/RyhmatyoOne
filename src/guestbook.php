9a0655d0254021bbb8688e905d7ccf11255749af
AL-student-11 <255115666+AL-student-11@users.noreply.github.com>
2026-03-09 04:31:41 +0200

    Adding guestbook page

---
+++ src/guestbook.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="fi">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="Vieraskirja">
+	<title>Vieraskirja</title>
+</head>
+<body>
+	<div class="entry">
+		<form>
+			<label for="name">Nimesi: </label>
+			<input id="name" type=text required></input>
+			<label for="email">Sähköposti: </label>
+			<input id="email" type=text></input>
+			<label for="message"></label>
+			<textarea id="message" rows=5></textarea>
+			<button type="submit">Lähetä</button>
+		</form>
+	</div>
+	<div class="entries">
+		<table>
+			<caption>Sivulla vierailleet</caption>
+			<tr>
+				<th>Aika</th>
+				<th>Nimi</th>
+				<th>Email</th>
+				<th>Viesti</th>
+			</tr>
+		</table>
+	</div>
+</body>
+</html>
